85692fe48d74aac0ae8959570b58a5d0137126c6
ubutputra <ubut31@gmail.com>
2019-12-08 20:59:13 +0700

    bagusin

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
 	<!-- Load File javascript config.js -->
 	<script src="js/config.js" type="text/javascript"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+	<style>
+		body {
+		color: white;
+		}
+</style>
 </head>
 <body>
 	<h1 align="center">Form Input </h1>
